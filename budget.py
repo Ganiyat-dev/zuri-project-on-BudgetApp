77806837a33eba895a056bff42d93666b8a31677
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2021-04-17 12:59:39 +0100

    deposit and balance Inquiry transaction in each category done

--- budget.py
+++ budget.py
@@ -1,31 +1,113 @@
-class budget_category:
-    account = []
-    balance = 0
-    name = ""
+class Budget:
+    def __init__(self, bCategory=["food","clothing","entertainment"], balance=[0,0,0]):
+        #balance at index 0 = foodBalance, index 1 = clothing and index 2 = entertainment
+        self.balance = balance
+        self.bCategory = bCategory
+        print ("These are the available categories: \n 1. Food \n 2. clothing \n 3. Entertainment")
+    def deposit(self):
+        selectedCategory = int(input("Please enter the Category option to continue \n"))
+        if selectedCategory not in [1,2,3]:
+            print("You must enter option 1, 2 or 3 to continue")
+            selectedCategory = int(input("Please enter the Category option to continue \n"))
+        if selectedCategory == 1:
+            amount = int(input("Enter amount to deposit: \n"))
+            self.balance[0] = self.balance[0] + amount
+            print("You have successfully deposited %d in your %s." % (self.balance[0], self.bCategory[0]))
+        elif selectedCategory == 2:
+            amount = int(input("Enter amount to deposit: \n"))
+            self.balance[1] = self.balance[1] + amount
+            print("You have successfully deposited %d in your %s." % (self.balance[1], self.bCategory[1]))
+        elif selectedCategory == 3:
+            amount = int(input("Enter amount to deposit: \n"))
+            self.balance[2] = self.balance[2] + amount
+            print("You have successfully deposited %d in your %s." % (self.balance[2], self.bCategory[2])) 
+        else:
+            print("You have selected an Invalid category option")
 
-    def __init__(self, name):
-        self.name =  name
-
-    def deposit(self, amount, description =""):
-        account.append({"amount": amount, "description": description})
-
-
-    def withdraw(self, amount, description = ""):
-        if amount > self.balance:
-            return False
+#withdraw money from each category respectively
+    def withdraw(self):
+         selectedCategory = int(input("Please Select an option to continue: \n"))
+         if selectedCategory not in [1,2,3]:
+            print("You must enter option 1, 2 or 3 to continue")
+            selectedCategory = int(input("Please enter the Category option to continue: \n"))
+         if selectedCategory == 1:
+            amount = int(input("Enter amount to withdraw: \n"))
+            if self.balance[0] < amount:
+                print("Insufficient Balance")
+            else:
+                self.balance[0] = self.balance[0] - amount
+            print("You have successfully withdrawed from your %s category." % (self.bCategory[0]))
+         
+         elif selectedCategory == 2:
+            amount = int(input("Enter amount to withdraw: \n"))
+            if self.balance[1] < amount:
+                print("Insufficient Balance")
+            else:
+                self.balance[1] = self.balance[1] - amount
+            print("You have successfully withdrawed from your %s category." % (self.bCategory[1]))
+         elif selectedCategory == 3:
+            amount = int(input("Enter amount to withdraw: \n"))
+            if self.balance[2] < amount:
+                print("Insufficient Balance")
+            else:
+                self.balance[2] = self.balance[2] - amount
+            print("You have successfully withdrawed from your %s category." % (self.bCategory[2]))
+         else:
+            print("You have selected an Invalid category option")
         
+    def check_balance(self):
+        selectedCategory = int(input("Please enter the Category option to continue: \n"))
+        if selectedCategory not in [1,2,3]:
+            print("You must enter 1, 2 or 3 to continue")
+            selectedCategory = int(input("please select a category option to continue: "))
+        if selectedCategory == 1:
+            print("Your %s BALANCE is: %s " % (self.bCategory[0], self.balance[0])) 
+        elif selectedCategory == 2:
+            print("Your %s BALANCE is: %s " % (self.bCategory[1], self.balance[1]))
+        elif selectedCategory == 3:
+            print("Your %s BALANCE is: %s " % (self.bCategory[2], self.balance[2]))
+        else:
+            print("Invalid category option")
+
+#transfer from current budget category(sending to receiving category)
+    def transfer_funds(self):
+        category = int(input("Select category to transfer from: \n"))
+        if category not in [1,2,3]:
+            print("You must enter 1, 2 or 3 to continue")
+            category = int(input("Select category to transfer from: \n"))
+        amount = int(input("Enter the amount: \n"))
+        rCategory = int(input("Enter category to transfer to \n"))
+        while category == rCategory:
+            print("You can not transfer to the same category")
+            rCategory = int(input("Enter category to transfer to: \n"))
+        if rCategory == 1:
+            self.balance[category] = self.balance[category] - amount 
+            self.balance[rCategory] = self.balance[rCategory] + amount
+            print("You have successfully transfered %d" %amount )
+        elif(rCategory == 2):
+            self.balance[category] = self.balance[category] - amount 
+            self.balance[rCategory] = self.balance[rCategory] + amount
+            print("You have successfully transfered %d" %amount )
+        elif(rCategory == 3):
+            self.balance[category] = self.balance[category] - amount 
+            self.balance[rCategory] = self.balance[rCategory] + amount
+            print("You have successfully transfered %d" %amount)
 
 
-    def check_balance(self, amount, description):
-        pass
+print ("Welcome to myBudgetApp")
+option = int(input("Select the transaction you would like to perform:  \n 1. to deposit \n 2. to withdraw \n 3. for balance Inquiry \n 4. to transfer \n"))
+while option not in (1,2,3,4):
+    print("Invalid option selected")
+    option = int(input("Enter a valid option to continue: "))
 
+budget = Budget()
+if option == 1:
+    budget.deposit()
+    pass
+elif option == 2:
+    budget.withdraw()
+elif option == 3:
+    budget.check_balance() 
+else:
+    budget.transfer_funds()
 
-    def transfer_funds(self, amount, description):
-        pass
-
-
-
-#deposit
-#withdraw
-#checkBalance
-#sendMoney
